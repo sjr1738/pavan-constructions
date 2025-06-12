ac49f12c0b7c55f73fa8ac993c1bdcac6aa7f8d2
Pratichi Ghosh <pratichighosh2003@gmail.com>
2025-06-12 21:33:28 +0530

    fix asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html') // Specify HTML location
-    }
+    emptyOutDir: true,
+    sourcemap: false
   }
 })
