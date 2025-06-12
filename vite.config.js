a68889a2db7b3a9e61f3878ffe394c0a1959dde3
Pratichi Ghosh <pratichighosh2003@gmail.com>
2025-06-12 21:18:25 +0530

    fix asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -12,8 +11,11 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
-      },
-    },
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   },
   server: {
     port: 3000,
